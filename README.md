cd3aae6d73b8703916b5bf555e5b79abedd0da69
haywalk <planethaywalk@aol.com>
2020-11-18 19:19:54 -0400

    made generic

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 This is the script I use to back up the [Space Hippie](https://www.spacehippie.ca) Minecraft server. I use Cron to run it every night at 22:00 local time.
 
-This is a pretty simple script. All you need to change to get it to work on your server are the locations of the world and backup files. You may also want to change the part that says "You're playing on the Space Hippie Minecraft server", but that won't affect its function.
+To adjust this for your server, simply change the variables at the top.
 
 For this to work, Minecraft has to be running in session 0 of Tmux.
--- mcbackup.sh
+++ mcbackup.sh
@@ -4,6 +4,9 @@
 # Hayden's Minecraft server backup script for GNU/Linux and TMUX
 #
 
+dir="/backup/location" # Abs. path to backup folder
+world="/world/location" # Abs. path to world folder
+
 # Announce backup
 tmux send-keys -t 0 "say Preparing to backup the server..." ENTER
 
@@ -21,11 +24,11 @@ day=$(date +%A)
 
 # Backup server to USB
 sleep 5s
-tar -cpvzf /media/mcbackup/minecraft-$day.tar.gz /home/hdw/MCServer/world
+tar -cpvzf $dir/minecraft-$day.tar.gz $world
 sleep 5s
 
 # Restart saving
 tmux send-keys -t 0 "save-on" ENTER
 
 # Announce completion
-tmux send-keys -t 0 "say Backup complete. Saving re-enabled. You're playing on the Space Hippie Minecraft server. [www.spacehippie.ca]" ENTER
+tmux send-keys -t 0 "say Backup complete. Saving re-enabled." ENTER
